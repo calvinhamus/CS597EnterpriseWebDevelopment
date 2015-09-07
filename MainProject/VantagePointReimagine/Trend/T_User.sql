cba720f887114fa801d1cf7099172a0fddf4ede4
calvinhamus <calvinhamus@u.boisestate.edu>
2015-09-07 14:18:13 -0600

    First table

---
+++ MainProject/VantagePointReimagine/Trend/T_User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[T_User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Username] VARCHAR(15) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [T_UserLevel] INT NULL
+)
