42f3d07c9879639e3e8e6b460e9d9b6f75c48070
calvinhamus <calvinhamus@u.boisestate.edu>
2015-09-07 14:24:17 -0600

    Re added DB

---
+++ MainProject/VantagePointReimagine/Trend.Database/T_User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[T_User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [username] VARCHAR(20) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL, 
+    [T_UserLevel] INT NOT NULL
+)
