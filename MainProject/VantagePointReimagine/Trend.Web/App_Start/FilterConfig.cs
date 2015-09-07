c0b0abccca5fee36140fdbaefb0c3c4e942fcd6e
calvinhamus <calvinhamus@u.boisestate.edu>
2015-09-07 14:10:10 -0600

    Project init and basic structure

---
+++ MainProject/VantagePointReimagine/Trend.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Trend.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MainProject/VantagePointReimagine/Trend.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trend.Web.MvcApplication" Language="C#" %>
